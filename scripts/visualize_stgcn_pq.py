e35fc651a372191276372588a36af15ffe0f478c
PotassiumWings <mumuyuyululu@126.com>
2024-04-04 03:11:13 +0800

    visualize

--- scripts/visualize_stgcn_pq.py
+++ scripts/visualize_stgcn_pq.py
@@ -5,18 +5,19 @@ from scripts_utils import *
 f = open("logs_pq", "r", encoding="utf-8")
 result = parse_f(f, pattern="python app.py", features=["--p", "--q"], default=["1", "1"], result_pattern="Test:")
 
-res = [[0 for j in range(7)] for i in range(7)]
-for i in range(7):
+N = 9
+res = [[0 for j in range(N)] for i in range(N)]
+for i in range(N):
     if result.get(str(i)) is None:
         continue
-    for j in range(7):
+    for j in range(N):
         # import pdb
         # pdb.set_trace()
         if result[str(i)].get(str(j)) is None:
             continue
         res[i][j] = result[str(i)][str(j)]["mae"]
-for i in range(1, 7):
-    for j in range(1, 7):
-        print(res[i][j], end=',')
+for i in range(1, N):
+    for j in range(1, N):
+        print(f"{res[i][j]:.2f}", end=',')
     print()
 print(result)
