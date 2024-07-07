ee7f4639d973b165f595c37158cb4454563b8fbc
PotassiumWings <mumuyuyululu@126.com>
2024-07-07 18:18:24 +0800

    fix dataset

--- preprocess/nycod.py
+++ preprocess/nycod.py
@@ -24,19 +24,22 @@ val_data = data[train_index:val_index, :, :]
 test_data = data[val_index:test_index, :, :]
 
 timestep = 48  # 4day
+horizon = 12
 
 
 def get_data(origin_data):
+    import pdb
+    pdb.set_trace()
     T, v, _ = origin_data.shape
-    res_x = np.asarray([origin_data[i:i + timestep, :, :] for i in range(T - timestep - 1)])
-    res_y = np.asarray([origin_data[i + timestep:i + timestep + 12, :, :] for i in range(T - timestep - 1)])
+    res_x = np.asarray([origin_data[i:i + timestep, :, :] for i in range(T - timestep - horizon - 1)])
+    res_y = np.asarray([origin_data[i + timestep:i + timestep + 12, :, :] for i in range(T - timestep - horizon - 1)])
     print(res_x.shape, res_y.shape)
     return res_x, res_y
 
 
-# x, y = get_data(train_data)
-# np.savez_compressed("../data/NYC-TOD5/train.npz", x=x, y=y)
-# x, y = get_data(val_data)
-# np.savez_compressed("../data/NYC-TOD5/val.npz", x=x, y=y)
+x, y = get_data(train_data)
+np.savez_compressed("../data/NYC-TOD5/train.npz", x=x, y=y)
+x, y = get_data(val_data)
+np.savez_compressed("../data/NYC-TOD5/val.npz", x=x, y=y)
 x, y = get_data(test_data)
 np.savez_compressed("../data/NYC-TOD5/test.npz", x=x, y=y)
